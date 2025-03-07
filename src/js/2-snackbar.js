c8765120cb3f2c4c362855e550c2063fd674261b
Stanislav-Zinovkin <stas14zinovkin@gmail.com>
2025-03-07 13:35:31 +0200

    Hw10-js-v0.4

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,43 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+const delayInput = document.querySelector('input[name="delay"]');
+const stateRadios = document.querySelectorAll('input[name="state"]');
+const submitButton = form.querySelector('button[type="submit"]');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const delay = Number(delayInput.value);
+  const state = [...stateRadios].find(radio => radio.checked).value;
+  createPromise(delay, state);
+});
+
+function createPromise(delay, state) {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  promise
+    .then(delay => {
+      iziToast.success({
+        title: 'Success',
+        message: `Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: 'Error',
+        message: `Rejected promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    });
+}
