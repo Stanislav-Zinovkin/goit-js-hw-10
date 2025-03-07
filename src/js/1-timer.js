beb5915cc2ed104796f744a41b4c0dbed746635c
Stanislav-Zinovkin <stas14zinovkin@gmail.com>
2025-03-07 12:27:15 +0200

    Hw10-js-v0.2

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,18 +1,14 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
-const datetimePicker = document.querySelector('#datetime - picker');
-flatpickr(datetimePicker, {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
-  },
-});
-const startButton = document.querySelector('[data-start]');
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const datetimePicker = document.querySelector('#datetime-picker');
 let userSelectedDate = null;
+const startButton = document.querySelector('[data-start]');
+const timerFields = document.querySelectorAll('.field .value');
+let timerInterval = null;
 
 flatpickr(datetimePicker, {
   enableTime: true,
@@ -22,15 +18,17 @@ flatpickr(datetimePicker, {
   onClose(selectedDates) {
     userSelectedDate = selectedDates[0];
     if (userSelectedDate < new Date()) {
-      window.alert('Please choose a valid date!');
+      iziToast.error({
+        title: 'Error',
+        message: 'Please choose a valid date!',
+        position: 'topCenter',
+      });
       startButton.disabled = true;
     } else {
       startButton.disabled = false;
     }
   },
 });
-const timerFields = document.querySelector('.field .value');
-let timerInterval = null;
 
 startButton.addEventListener('click', () => {
   if (userSelectedDate) {
@@ -45,8 +43,10 @@ startButton.addEventListener('click', () => {
         clearInterval(timerInterval);
         updateTimer(0, 0, 0, 0);
         enableControls();
+        showCompletionMessage();
         return;
       }
+
       const { days, hours, minutes, seconds } = convertMs(remainingTime);
       updateTimer(days, hours, minutes, seconds);
     }, 1000);
@@ -65,6 +65,28 @@ function addLeadingZero(value) {
 }
 
 function enableControls() {
-  startButton.disabled = false;
+  startButton.disabled = true;
   datetimePicker.disabled = false;
+}
+
+function showCompletionMessage() {
+  iziToast.success({
+    title: 'Completed',
+    message: 'The timer has finished!',
+    position: 'topCenter',
+  });
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
 }
